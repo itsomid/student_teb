b62aab38bac893808005afb2a9def9dd9d1bd655
Mehdi Rajabi <mehdints@gmail.com>
2024-06-19 15:38:36 +0330

    feat: assign product access after order completed

--- src/app/Models/ProductAccess.php
+++ src/app/Models/ProductAccess.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
+use App\Enums\ProductAccessType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductAccess extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'order_item_id', 'product_id', 'user_id', 'effective_from_datetime', 'effective_to_datetime', 'access_reason_type'
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'access_reason_type' => ProductAccessType::class
+        ];
+    }
 }
--- src/app/Services/OrderService.php
+++ src/app/Services/OrderService.php
@@ -3,7 +3,8 @@
 namespace App\Services;
 
 use App\Enums\OrderStatusEnum;
-use App\Models\Order;
+use App\Enums\ProductAccessType;
+use App\Models\ProductAccess;
 use App\Models\User;
 use App\ShoppingCart\CartAdaptor;
 use App\ShoppingCart\Contract\CartItemInterface;
@@ -42,24 +43,41 @@ class OrderService
                 $item->getModel()->packages->map(function ($course) use($order, $amount, $sum) {
                     $amount_temp = (int) (($course->product->price*$amount)/$sum);
 
-                    $order->items()->create([
+                    $itemModel = $order->items()->create([
                         'product_id' => $course->product_id,
                         'final_price' => $amount_temp,
                         'product_price' => $course->product->original_price,
                     ]);
+                    $this->accessProduct($course->product_id, $itemModel->id);
+
                 });
             }
 
-            $order->items()->create([
+            $itemModel = $order->items()->create([
                 'product_id' => $item->product_id,
                 'final_price' => $item::IS_PACKAGE ? 0 : $item->getCalcPrice(),
                 'product_price' => $item->getModel()->product->original_price,
             ]);
 
+            $this->accessProduct($item->product_id, $itemModel->id);
             CartAdaptor::remove($item->product_id);
 
         });
+    }
 
-
+    /**
+     * @param int $productId
+     * @param int $orderItemId
+     * @return void
+     */
+    public function accessProduct(int $productId, int $orderItemId): void
+    {
+        ProductAccess::query()
+            ->create([
+                'product_id' => $productId,
+                'user_id' => $this->userId,
+                'access_reason_type' => ProductAccessType::BOUGHT,
+                'order_item_id' => $orderItemId
+            ]);
     }
 }
