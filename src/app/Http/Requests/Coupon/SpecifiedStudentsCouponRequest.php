008ee55d16bfa2d57e852ed6dbce9ab88c466d99
Mohammad Kasiri <mohammad.kasirey@gmail.com>
2024-07-13 00:01:49 +0330

    [Hot Fix] Make Product ID Optional

--- src/app/Http/Requests/Coupon/SpecifiedStudentsCouponRequest.php
+++ src/app/Http/Requests/Coupon/SpecifiedStudentsCouponRequest.php
@@ -54,7 +54,7 @@ class SpecifiedStudentsCouponRequest extends FormRequest
             'discount_percentage'   => ['nullable', 'numeric', 'required_without:discount_amount'],
             'discount_amount'       => ['nullable', 'numeric', 'required_without:discount_percentage'],
             'expired_at'            => ['nullable', 'date'],
-            'product_ids'           => ['required', 'array'],
+            'product_ids'           => ['array'],
             'is_one_time'           => ['required', 'boolean'],
         ];
     }
