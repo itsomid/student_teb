ba316f6a5ebcb5f6918d7ee0a59dfeb61499234f
Mehdi Rajabi <mehdints@gmail.com>
2024-07-17 15:02:19 +0330

    feat: Implement service to get student's purchased product ids

--- src/app/Services/StudentAccountService.php
+++ src/app/Services/StudentAccountService.php
@@ -2,12 +2,50 @@
 
 namespace App\Services;
 
+use App\DTO\StudentAccount\ProductAccessResponseDTO;
+use App\Enums\ProductAccessType;
 use App\Models\Account;
+use App\Models\ProductAccess;
+use Illuminate\Database\Eloquent\Builder;
 
 class StudentAccountService
 {
     public function getBalance(int $userId): int
     {
         return Account::getStudentBalance($userId);
+    }
+
+    /**
+     * @param int $userId
+     * @param bool $excludeFree
+     * @return array
+     */
+    public function getPurchasedProductIds(int $userId, bool $excludeFree = false): array
+    {
+        $productAccesses = ProductAccess::query()
+            ->where('user_id', $userId)
+            ->where(fn (Builder $query) =>
+                $query->where('effective_from_datetime', '>=', now())
+                        ->orWhereNull('effective_from_datetime')
+            )
+            ->where(fn (Builder $query) =>
+            $query->where('effective_to_datetime', '>=', now())
+                ->orWhereNull('effective_to_datetime')
+            );
+
+        if ($excludeFree) {
+            $productAccesses->where('access_reason_type', ProductAccessType::BOUGHT);
+        }
+
+        $productAccesses = $productAccesses->get();
+
+        return $productAccesses->map(fn($access) =>
+            resolve(ProductAccessResponseDTO::class)
+            ->setUserId($access->user_id)
+            ->setProductId($access->product_id)
+            ->setEffectiveFromDateTime($access->effective_from_datetime)
+            ->setEffectiveToDateTime($access->effective_to_datetime)
+            ->setAccessReasonType($access->access_reason_type)
+        )->toArray();
     }
 }
