c167f49fc5baf5847cf23e72fd0eaf6332077d94
Omid <o.shabani@hotmail.com>
2024-07-20 15:52:52 +0330

    fix missed code

---
+++ src/app/ShoppingCart/Exceptions/CouponNotUsableExpireNotApplicable.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\ShoppingCart\Exceptions;
+
+use Exception;
+
+class CouponNotUsableExpireNotApplicable extends Exception
+{
+}
---
+++ src/app/ShoppingCart/Exceptions/CouponNotUsableProductNotApplicable.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\ShoppingCart\Exceptions;
+
+use Exception;
+
+class CouponNotUsableProductNotApplicable extends Exception
+{
+//    protected $productIds;
+//
+//    public function __construct(array $productIds)
+//    {
+//        parent::__construct("Coupon not usable for products with IDs: " . implode(', ', $productIds));
+//        $this->productIds = $productIds;
+//    }
+//
+//    public function getProductIds()
+//    {
+//        return $this->productIds;
+//    }
+}
