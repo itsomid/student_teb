43a924aa87622c8f37353dce7564a33dab2ffc32
Mohammad Kasiri <mohammad.kasirey@gmail.com>
2024-08-05 13:07:50 +0330

    [Task 2899] Add Chart and Filter

--- src/app/Http/Controllers/Admin/TransactionReportController.php
+++ src/app/Http/Controllers/Admin/TransactionReportController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Functions\DateFormatter;
 use App\Http\Controllers\Controller;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
@@ -13,24 +14,39 @@ class TransactionReportController extends Controller
 {
     public function index()
     {
-        $time_span       = request()->input('time_span',7);
+        if(request()->input('from_date') xor request()->input('to_date'))
+        {
+            return redirect()->route('admin.transaction.report.index')->withErrors(['from_date'=>'یکی از تاریخ ها نمیتواند خالی باشد']);
+        }
+
+
         $transaction_type= request()->input('type','deposit');
         $deposit_type    = request()->input('deposit_type','');
-        $today = Carbon::now();
+        $from_date       = request()->input('from_date','');
+        $to_date         = request()->input('to_date','');
+        $today           = Carbon::now();
 
-        // Get transactions for the last 7 days
+        // Get transactions for chart
         $transactions = Transaction::query()
             ->select(
                 DB::raw('DATE(created_at) as date'),
                 'transaction_type',
                 DB::raw('SUM(amount) as total_amount')
             )
-            ->whereBetween('created_at', [$today->copy()->subDays($time_span), $today])
             ->where('transaction_type', $transaction_type)
             ->when($transaction_type == 'deposit' && $deposit_type, function ($query) use ($deposit_type) {
                 $query->whereHas('deposit', function ($query) use ($deposit_type) {
                     $query->where('deposit_type', $deposit_type);
                 });
+            })
+            ->when($from_date, function ($query) use ($from_date) {
+                $query->where('created_at', '>=', DateFormatter::format($from_date));
+            })
+            ->when($to_date, function ($query) use ($to_date) {
+                $query->where('created_at', '<=', DateFormatter::format($to_date));
+            })
+            ->when(!$from_date && !$to_date, function ($query) use ($today) {
+                $query->whereBetween('created_at', [$today->copy()->subDays(7), $today]);
             })
             ->groupBy('date', 'transaction_type')
             ->orderBy('date', 'asc')
@@ -42,6 +58,11 @@ class TransactionReportController extends Controller
         foreach ($transactions as $transaction) {
             $chartData[$transaction->date] = $transaction->total_amount;
         }
+
+
+        $time_span=  ($from_date || $to_date)
+            ? (int) Carbon::parse($from_date)->diffInDays($to_date) + 1
+            : 7;
 
 
         $dates = [];
