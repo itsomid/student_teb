1f90729da115573304c6b35a48ca24a6ed42b6cd
Mohammad Kasiri <mohammad.kasirey@gmail.com>
2024-08-18 15:10:12 +0330

    [Task 2989] Sales Report  Based on Category

--- src/app/Http/Controllers/Admin/SalesReportByCategory.php
+++ src/app/Http/Controllers/Admin/SalesReportByCategory.php
@@ -4,13 +4,40 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\OrderItem;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SalesReportByCategory extends Controller
 {
     public function index()
     {
         $categories= Category::query()->active()->orderBy('type')->get();
-        return view('dashboard.sales_report_by_category.index')->with('categories',$categories);
+
+        if(!request()->has('categories'))
+            return view('dashboard.sales_report_by_category.index')->with('categories',$categories);
+
+
+        $product_ids = DB::table('category_product')
+            ->select('product_id')
+            ->whereIn('category_id', request()->input('categories'))
+            ->pluck('product_id')
+            ->toArray();
+
+        $products = Product::query()->select('id','name')->whereIn('id', $product_ids)->get();
+
+        $totalPaidAmountByProduct = OrderItem::query()
+            ->select('product_id', DB::raw('SUM(final_price) as total_price'))
+            ->whereIn('product_id', $product_ids)
+            ->groupBy('product_id')
+            ->get();
+
+        $categories= Category::query()->active()->orderBy('type')->get();
+
+        return view('dashboard.sales_report_by_category.index')
+            ->with('categories',$categories)
+            ->with('products',$products)
+            ->with('totalPaidAmountByProduct',$totalPaidAmountByProduct);
     }
 }
--- src/resources/views/dashboard/sales_report_by_category/index.blade.php
+++ src/resources/views/dashboard/sales_report_by_category/index.blade.php
@@ -11,7 +11,7 @@
                         </div>
                         <form action="{{route('admin.sales-report-by-category.form')}}" method="get"  class="row mt-5" >
                             <div class="row">
-                                <div class="col-md-6">
+                                <div class="col-md-5">
                                     <div class="form-group">
                                         <label for="created_at">از تاریخ</label>
                                         <input required
@@ -23,7 +23,7 @@
                                                placeholder="جهت درج تاریخ کلیک کنید">
                                     </div>
                                 </div>
-                                <div class="col-md-6">
+                                <div class="col-md-5">
                                     <div class="form-group">
                                         <label for="created_at">تا تاریخ</label>
                                         <input required
@@ -35,13 +35,17 @@
                                                placeholder="جهت درج تاریخ کلیک کنید">
                                     </div>
                                 </div>
+                                <div class="col-md-2">
+                                    <label for=""> </label>
+                                    <button class="btn btn-success w-100 ">دریافت گزارش</button>
+                                </div>
                             </div>
                             <div class="row">
                                 @foreach($categories as $category)
                                     <div class="col-md-4">
                                         <div class="form-check my-3 d-block">
                                             <label class="form-check-label">
-                                                <input class="form-check-input" type="checkbox" name="roles[]" value="{{$category->id}}">
+                                                <input class="form-check-input" type="checkbox" name="categories[]" value="{{$category->id}}">
                                                 {{$category->name}}
                                                 <span class="form-check-sign">
                                         <span class="check">
@@ -55,6 +59,39 @@
                         </form>
                     </div>
                 </div>
+                @if(request()->has('categories'))
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card-title header-elements">
+                            <h5 class="m-0 me-2"> آمار فروش براساس دسته بندی </h5>
+                        </div>
+                        <div class="row">
+                            <div class="table-responsive text-nowrap">
+                                <table class="table">
+                                    <thead>
+                                        <th>شناسه محصول</th>
+                                        <th>نام محصول</th>
+                                        <th>مجموع مبلغ فروش</th>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($totalPaidAmountByProduct as $amount)
+                                        <tr>
+                                            <th>{{$amount->product_id}}</th>
+                                            <th>{{$products->where('id', $amount->product_id)->first()->name}}</th>
+                                            <th>
+                                                {{$amount->total_price}}
+                                                تومان
+                                            </th>
+                                        </tr>
+                                    @endforeach()
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                 @endif
             </div>
         </div>
     </div>
