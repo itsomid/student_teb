4dda91e34d9f1b19bc8468c8982466e080f6246b
Mehdi Rajabi <mehdints@gmail.com>
2024-09-18 15:14:31 +0330

    fix: correct price

--- src/database/factories/OrderItemFactory.php
+++ src/database/factories/OrderItemFactory.php
@@ -19,9 +19,10 @@ class OrderItemFactory extends Factory
         return [
             'order_id' => $this->faker->numberBetween(1, 50), // Assume 100 orders
             'product_id' => $this->faker->numberBetween(1, 100), // Assume 100 products
-            'final_price' => $this->faker->randomFloat(2, 10, 500),
-            'product_price' => $this->faker->randomFloat(2, 10, 500),
-            'discount_price' => $this->faker->randomFloat(2, 0, 100),
+            'final_price' => $finalPrice = $this->faker->randomElement([$this->faker->numerify('#000000'), $this->faker->numerify('##00000'), $this->faker->numerify('###00000')]),
+            'final_price_without_vat' => $finalPrice - ($finalPrice * 0.1),
+            'product_price' => $this->faker->randomElement([$this->faker->numerify('#000000'), $this->faker->numerify('##00000'), $this->faker->numerify('###00000')]),
+            'discount_price' => $this->faker->randomElement([0, 0, $this->faker->numerify('#0000')]),
             'user_gift_amount' => $this->faker->randomElement([10000, 500000, 0, 300000, 0]),
         ];
     }
